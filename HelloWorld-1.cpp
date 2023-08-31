b404f3bfcd9327aa3c261ad982680e7b5d84c2d1
Ahmad Qureshi <m.ahmad.qureshi1217@gmail.com>
2023-08-31 10:55:23 -0400

    Adding author name

--- HelloWorld-1.cpp
+++ HelloWorld-1.cpp
@@ -1,5 +1,5 @@
 // Simple C++ program to display "Hello World"
-// Author: Ahmad Qureshi <hd3563@wayne.edu>
+// Author: Ahmad Qureshi <m.ahmad.qureshi1217@gmail.com>
 
 // Header file for input output functions 
 #include<iostream>  
